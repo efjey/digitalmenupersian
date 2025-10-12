8f4da66604d388bffdd52b95d1ba93a44f58f27e
fj <parsafiruzjaei@gmail.com>
2025-10-12 15:28:57 +0330

    'userSectionCreated'

--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import start
+from .views import * 
 
 urlpatterns = [  
-    path('', start)
+    path('', start, name="home"),
+    path('login_user/', login_user, name="login_user"),
+    path('logout_user/', logout_user, name="logout_user"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def start(request):
     all_products = Product.objects.all()
     
     return render(request, 'index.html', {'products':all_products})
 
+def login_user(request):
+    if request.method=="POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("با موفقیت وارد شدید"))
+            return redirect("home")
+        else:
+            messages.success(request, ("نام کاربری یا رمز عبور اشتباه است"))
+            return redirect("login_user")
+
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "با موفیقت خارج شدید")
+    return render(request, "index.html", {})
